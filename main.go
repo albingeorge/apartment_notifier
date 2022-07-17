862418b939db550a589879d4b6e38303376afb0f
Albin George <albinin0002@gmail.com>
2022-07-17 09:21:46 +0200

    Handle http failure, change to alert, pass area in alert

--- main.go
+++ main.go
@@ -12,23 +12,25 @@ import (
 )
 
 func main() {
-	go notifyNewApartments("https://www.pararius.com/apartments/amsterdam/0-1500")
+	go notifyNewApartments("https://www.pararius.com/apartments/amsterdam/0-1500", "Amsterdam")
 
-	go notifyNewApartments("https://www.pararius.com/apartments/utrecht/0-1500")
+	go notifyNewApartments("https://www.pararius.com/apartments/utrecht/0-1500", "Utrecht")
+
+	go notifyNewApartments("https://www.pararius.com/apartments/amersfoort/0-1500", "Amersfoot")
 	select {}
 }
 
-func notifyNewApartments(url string) {
+func notifyNewApartments(url string, area string) {
 	ctx := context.Background()
 
-	compareApartments(ctx, url)
+	compareApartments(ctx, url, area)
 
 	for range time.Tick(time.Minute * 2) {
-		compareApartments(ctx, url)
+		compareApartments(ctx, url, area)
 	}
 }
 
-func compareApartments(ctx context.Context, url string) {
+func compareApartments(ctx context.Context, url string, area string) {
 	// Get stored top apartment
 	latestApartment := getInstance().redis.HGet(ctx, "latest_apartments", url).Val()
 	fmt.Println("\nLATEST apartment: ", latestApartment)
@@ -40,7 +42,7 @@ func compareApartments(ctx context.Context, url string) {
 	if len(newApartments) > 0 && newApartments[0] != latestApartment {
 		// Print diff if not
 		fmt.Println("New apartments found!")
-		printLatestApartments(latestApartment, newApartments)
+		printLatestApartments(latestApartment, newApartments, area)
 
 		// Update the stored top apartment
 		getInstance().redis.HSet(ctx, "latest_apartments", url, newApartments[0])
@@ -55,6 +57,9 @@ func getNewApartments(url string) []string {
 
 	if err != nil {
 		log.Println("HTTP fetch failed: ", url)
+		// IF http fetch failed, we should return immediately, else the defer statement will fail
+		// Alternative is to return err as well and handle gracefully in the calling function
+		return []string{}
 	}
 
 	defer resp.Body.Close()
@@ -82,8 +87,8 @@ func getNewApartments(url string) []string {
 	return apartments
 }
 
-func printLatestApartments(oldLatestApartment string, newApartments []string) {
-	notifyMessage := "New apartments:\n"
+func printLatestApartments(oldLatestApartment string, newApartments []string, area string) {
+	notifyMessage := ""
 	fmt.Println("New apartments:")
 	for _, apartment := range newApartments {
 		if apartment == oldLatestApartment {
@@ -92,5 +97,5 @@ func printLatestApartments(oldLatestApartment string, newApartments []string) {
 		notifyMessage = notifyMessage + apartment + "\n"
 		fmt.Println(apartment)
 	}
-	notify.Notify("Apartment notifier", "New apartment found", notifyMessage, "path/to/icon.png")
+	notify.Alert("Apartment notifier", "New apartment found in "+area, notifyMessage, "path/to/icon.png")
 }
